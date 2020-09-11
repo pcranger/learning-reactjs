e8756cd68a3b6df6c570ad750008ee58ae3efed3
pcranger <52710878+pcranger@users.noreply.github.com>
2020-09-11 20:19:57 +0700

    referencing a text

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+
+function getButtonText(){
+    return 'Click on me!';
+}
+
 // Create a react component
 const App = function(){
-    return <div>Hi there!</div>;
+    const buttonText = {text: 'click me'};
+    return (
+        <div>
+        <label className = "label" for= "name">
+        Enter name:                 
+         </label>
+         <input id = "name" type ="text"/>
+         <button style={{backgroundColor: 'blue', color : 'white'}}>
+             {buttonText.text} 
+        </button>
+        </div>
+    );
 }
 
 // Take react component and show on screen
