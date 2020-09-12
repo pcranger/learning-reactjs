be42210d90109fa7b988d189a591aec234e8faec
pcranger <52710878+pcranger@users.noreply.github.com>
2020-09-12 23:37:34 +0700

    styling

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,17 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
+
+const seasonConfig = { //seasonConfig is an object with keys summer and winter and their values
+    summer: {
+        text:'Lets hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, it is chilly',
+        iconName: 'snowflake'
+    }
+}
 const getSeason = (lat,month) => {
     if (month > 2 && month < 9){
         return lat > 0 ? 'summer' : 'winter'; //(lat > 0): in the northen hemisphere
@@ -9,9 +21,18 @@ const getSeason = (lat,month) => {
 }
 
 const SeasonDisplay = (props) => {
-    const season = getSeason(props.lat, new Date().getMonth())
-    
-    console.log(season);
-    return <h1>{season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'}</h1>
+    const season = getSeason(props.lat, new Date().getMonth()) //season = winter or summer
+    const {text, iconName} = seasonConfig[season]
+
+    return (
+    <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`}/>
+        <h1>{text}</h1>
+        <i className={`icon-right massive ${iconName} icon`}/>
+
+    </div>
+    )
 }
 export default SeasonDisplay
+
+//`${icon} icon` string template to seperate variable and text
