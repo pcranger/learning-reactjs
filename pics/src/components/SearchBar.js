fe7db26e0ba8997b9d187654847a10e198b378f5
pcranger <52710878+pcranger@users.noreply.github.com>
2020-09-13 17:01:22 +0700

    understand function of state

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class SearchBar extends React.Component{
+    state = { term: ''};
+
+    onFormSubmit(event){
+        event.preventDefault();//prevent refreshing page
+        console.log(this.state.term)
+    }
+
+
+    render(){
+        return (
+        <div className = "ui segment">
+            <form onSubmit={this.onFormSubmit}className = "ui form">
+                <div className = "field">
+                    <label>Image Search</label>
+                <input type = "text" value={this.state.term} onChange = {(e) => this.setState({term: e.target.value})}/>
+                </div>   
+            </form>    
+        </div>
+        )
+    }
+}
+export default SearchBar
+
+//this.onInPutChange : a callback function (called some time in the future)
+//this.onInPutChange() : this function will be called anytime component is rerendered
+
+//central data in React component(state), not HTML DOM(<input value= "data">)
+//example user press 'a': a is then stored in state, then get displayed in <input>
+//storing in state can have uses like type uppercase only, number only
